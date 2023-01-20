eb6d5a38411f077ca5d731fa9575f6773ee51821
Henry Houle <hhoule@usc.edu>
2023-01-20 03:20:28 +0000

    Finished part 1

--- split.cpp
+++ split.cpp
@@ -9,15 +9,125 @@ To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
-
+#include <cstddef>
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+void appendOdd(Node* in, Node*& odds);
+
+void appendEven(Node* in, Node*& evens);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  Node* temp;
+//if there's no data
+  if(in == NULL)
+    return;
+
+  //if there's only one data point left
+  if(in->next == NULL)
+  {
+    if(in->value%2 == 1)
+    {
+      appendOdd(in, odds);
+      in = NULL;
+    }
+    else if(in->value%2 == 0)
+    {
+      appendEven(in, evens);
+      in = NULL;
+    }
+    return;
+  }
+
+  //If input list is longer than one data point
+
+  
+  //findFirstOdds(in, odds);
+  //findFirstEven(in, evens);
+
+  else if(in->value % 2 == 1)
+  {
+    temp = in;
+    in = in->next;
+    appendOdd(temp, odds);
+  }
+  else if(in->value%2 == 0)
+  {
+    temp = in;
+    in = in->next;
+    appendEven(temp, evens);
+  }
+
+  split(in, odds, evens);
+
 }
 
+//Adds odd number to end of odds list
+void appendOdd(Node* in, Node*& odds)
+{
+  if(odds == NULL)
+  {
+    odds = in;
+    odds->next = NULL;
+    return;
+  }
+
+  if(odds->next == NULL)
+  {
+    odds->next = in;
+    odds->next->next = NULL;
+    return;
+  }
+  else
+    appendOdd(in, odds->next);
+}
+
+//Adds even number to end of evens list
+void appendEven(Node* in, Node*& evens)
+{
+  if(evens == NULL)
+  {
+    evens = in;
+    evens->next = NULL;
+    return;
+  }
+
+  if(evens->next == NULL)
+  {
+    evens->next = in;
+    evens->next->next = NULL;
+    return;
+  }
+  else
+    appendEven(in, evens->next);
+}
+
+
 /* If you needed a helper function, write it here */
+
+//Finds the position for odds
+/*void findFirstOdd(Node* in, Node*& odds)
+{
+  if(in->value&2 == 1)
+  {
+    odds = in;
+    return;
+  }
+  else
+    findFirstOdd(in->next, odds);
+}
+
+//find the position of first even
+void findFirstEven(Node* in, Node*& evens)
+{
+  if(in->value&2 == 0)
+  {
+    odds = in;
+    return;
+  }
+  else
+    findFirstEven(in->next, evens);
+}*/
--- test_split.cpp
+++ test_split.cpp
@@ -8,10 +8,48 @@ Create linked lists and split them with your split() function.
 You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
-
+#include <iostream>
+#include <cstddef>
 #include "split.h"
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+ Node n10(11, NULL);
+  Node n9(78, &n10);
+  Node n8(0, &n9);
+  Node n7(21, &n8);
+  Node n6(4, &n7);
+  Node n5(5, &n6);
+  Node n4(5, &n5);
+  Node n3(3, &n4);
+  Node n2(2, &n3);
+  Node n1(1, &n2);
 
+  Node* head = &n1;
+
+  Node* odds = NULL;
+  Node* evens = NULL;
+
+  split(head, odds, evens);
+
+  Node* temp = odds;
+  while(temp != NULL)
+  {
+    cout << temp->value << " ";
+    temp = temp->next;
+  }
+  cout << endl;
+  temp = evens;
+  while(temp != NULL)
+  {
+    cout << temp->value << " ";
+    temp = temp->next;
+  }
+  cout << endl;
+
+  if(head == NULL)
+    cout << "Head was properly emptied" << endl;
+
+  return 0;
 }
